3cba09a7f4a9c7c51642bd6c6a18043c915bfc66
Jimmy Thrasher <jimmy@jimmythrasher.com>
2017-06-03 21:13:46 -0400

    Get rtpmidi.js up to ferrying MIDI messages back and forth

--- rtpmidi.js
+++ rtpmidi.js
@@ -1,37 +1,47 @@
 var rtpmidi = require('rtpmidi');
+var midi = require("midi");
 
-var sessions = rtpmidi.manager.getSessions(),
-  remoteSessions = rtpmidi.manager.getRemoteSessions();
-
-rtpmidi.manager.on('sessionAdded', function(event) {
-  console.log('A local session was created');
-});
-
-rtpmidi.manager.on('sessionRemoved', function(event) {
-  console.log('A local session was removed');
-});
+rtpmidi.manager.on('sessionAdded', function(event) { console.log('A local session was created'); });
+rtpmidi.manager.on('sessionRemoved', function(event) { console.log('A local session was removed'); });
 
 var session = rtpmidi.manager.createSession({
-  localName: 'My RTPMidi Session',
-  bonjourName: 'Node Midi Client',
+  localName: 'RTPMidi Session',
+  bonjourName: 'Room of Sound RTP MIDI',
   port: 5006
 });
 
-session.on('streamAdded', function(event) {
-  console.log('The stream "' + event.stream.name + '" was added to the session "' + session.localName +'"');
-});
-session.on('streamRemoved', function(event) {
-  console.log('The stream "' + event.stream.name + '" was removed from the session "' + session.localName +'"');
+session.on('streamAdded', function(event) { console.log('The stream "' + event.stream.name + '" was added to the session "' + session.localName +'"'); });
+session.on('streamRemoved', function(event) { console.log('The stream "' + event.stream.name + '" was removed from the session "' + session.localName +'"'); });
+rtpmidi.manager.on('remoteSessionAdded', function(event) { console.log('A remote session was discovered: ' + event.remoteSession.name); });
+rtpmidi.manager.on('remoteSessionRemoved', function(event) { console.log('A remote session disappered'); });
+
+// connect to a remote session by Bonjour name
+var connectNamed = function(name) {
+  var remotes = rtpmidi.manager.getRemoteSessions();
+  for (var i = 0; i < remotes.length; i++) {
+    if (remotes[i].name === name) {
+      session.connect(remotes[i]);
+    }
+  }
+};
+
+// discover Bonjour MIDI services. Will display names suitable for connectNamed above
+rtpmidi.manager.startDiscovery();
+
+// create local MIDI virtual ports
+input = new midi.input();
+output = new midi.output();
+input.openVirtualPort("Virtual Input");
+output.openVirtualPort("Virtual Output");
+
+// MIDI input (local MIDI -> RTP)
+input.on('message', function(deltaTime, message) {
+  session.sendMessage(deltaTime, message);
 });
 
-rtpmidi.manager.on('remoteSessionAdded', function(event) {
-  console.log('A remote session was discovered: ' + event.remoteSession);
-  console.log('Connecting...');
-  //session.connect(event.remoteSession);
+// MIDI output (RTP -> local MIDI)
+session.on('message', function(deltaTime, message) {
+  // message is a Buffer so we convert it to an array to pass it to the midi output.
+  var commands = Array.prototype.slice.call(message, 0);
+  output.sendMessage(commands);
 });
-
-rtpmidi.manager.on('remoteSessionRemoved', function(event) {
-  console.log('A remote session disappered');
-});
-
-
